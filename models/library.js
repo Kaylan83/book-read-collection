3e0413b524acb577c38d8d5da957010b5b736d41
Murad Atta <murad.atta@gmail.com>
2020-08-18 20:41:49 -0400

    saving changes

--- models/library.js
+++ models/library.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
             }
         },
         author: {
-            type: DataTypes.String,
+            type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 len: [1]
